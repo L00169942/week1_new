8ec17a3facdc494c9e1aaf140cfc7dc0113dbb24
Dali <dalimol.abraham@digitalmesh.com>
2021-09-24 22:59:13 +0530

    first commit

---
+++ tut_pkg/q1.py
@@ -0,0 +1,19 @@
+# .........................
+# Q1 file
+# .........................
+print("\t"*2+"Title")
+print('Hello');
+print("Hello World")
+a = 5
+b = 12.3456789
+c = a + b
+# d = "A number" + b
+e = a+ \
+    b+ \
+    c
+f = 2
+g = a/f
+print(a)
+print(b)
+print(c)
+print(g)
