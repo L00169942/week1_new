0d5121341d974b986f99fc0b0f221cada27229a9
Dali <dalimol.abraham@digitalmesh.com>
2021-10-29 20:09:30 +0100

    Week1

--- tut_pkg/q4.py
+++ tut_pkg/q4.py
@@ -2,27 +2,26 @@
 # Mathematical function
 # ............................
 
-ans_1 = 6+8/2-1
+ans_1 = 6+8/2-1  # expecting result = 9 bcz 1 => 8/2 = 4 , 2 => 4 - 1 = 3, 3 =>6 + 3
 print(ans_1)
 
-ans_2 = 1+1 - 1 + 2/2
+ans_2 = 1+1 - 1 + 2/2  # expecting result = 2 bcz 1 => 2/2 , 2 => -1 + 1, 3 => 2 + 0
 print(ans_2)
 
-ans_3 = 1.1 + 5/2
+ans_3 = 1.1 + 5/2  # expecting result = 3.6 bcz 1 => 5/2 = 2.5, 2 => 2.5 + 1.1 = 3.6
 print(ans_3)
 
-ans_4 = 9.2/3
+ans_4 = 9.2/3  # expecting result = 3.06
 print(ans_4)
 
-ans_5 = 9.2%3
+ans_5 = 9.2%3  # expecting result = 0.12
 print(ans_5)
 
-ans_6 = 1*2 + 3*5%4
+ans_6 = 1*2 + 3*5%4 # expecting result = 5 bcz 1 => 5%4 = 1, 2 => 3*1 = 3, 3 => 3 + 2
 print(ans_6)
 
-
-ans_7 = 1 + 8 % 3 * 2-9
+ans_7 = 1 + 8 % 3 * 2-9  # expecting result = -4 bcz 1 => 8%3 = 1, 2 => 1*-5 = -5, 3 => -5 + 1
 print(ans_7)
 
-ans_8 = 6 + (25 % (16 -11))
+ans_8 = 6 + (25 % (16 -11)) # expecting result = 6 bcz 1 => 16-11 = 5, 2 => 25%5 = 0
 print(ans_8)
